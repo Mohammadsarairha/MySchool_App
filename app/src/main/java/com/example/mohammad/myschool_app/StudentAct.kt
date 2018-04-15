2ebec7d6f0bf162748cd9f7d9b5af15700dfb1fa
Mohammad <moahmmadsarairha@gmail.com>
2018-04-15 21:03:50 +0300

    com

--- app/src/main/java/com/example/mohammad/myschool_app/StudentAct.kt
+++ app/src/main/java/com/example/mohammad/myschool_app/StudentAct.kt
@@ -2,6 +2,12 @@ package com.example.mohammad.myschool_app
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.Volley
+import kotlinx.android.synthetic.main.activity_student.*
 
 class StudentAct : AppCompatActivity() {
 
@@ -9,6 +15,25 @@ class StudentAct : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_student)
 
+        var url:String=Web_info.based_Url+"bus_students_get.php"
 
+        var studentlist=ArrayList<String>()
+
+        var rq= Volley.newRequestQueue(this)
+
+        var sr= JsonArrayRequest(Request.Method.GET,url,
+                null,Response.Listener { response ->
+
+            for (x in 0..response.length()-1){
+                studentlist.add(response.getJSONObject(x).getString("bus_name+")+"\n"+response.getJSONObject(x).getString("st_name+"))
+            }
+
+            var adp=ArrayAdapter(this,android.R.layout.simple_list_item_1,studentlist)
+            student_lv.adapter=adp
+        },
+                Response.ErrorListener { error ->  }
+                )
+
+        rq.add(sr)
     }
 }

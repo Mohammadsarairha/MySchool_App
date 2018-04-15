cb71675697dda5000ca0e5e8b0ad480caab9f28a
Mohammad <moahmmadsarairha@gmail.com>
2018-04-15 20:34:27 +0300

    schoolapp

--- app/src/main/java/com/example/mohammad/myschool_app/LoginAct.kt
+++ app/src/main/java/com/example/mohammad/myschool_app/LoginAct.kt
@@ -2,6 +2,10 @@ package com.example.mohammad.myschool_app
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginAct : AppCompatActivity() {
@@ -12,7 +16,20 @@ class LoginAct : AppCompatActivity() {
 
         login_button.setOnClickListener {
 
-            
-        }
+            var url:String=Web_info.based_Url+"user_type_select.php"
+
+            var rq= Volley.newRequestQueue(this)
+
+            var sr=object: StringRequest(Request.Method.GET,url,
+                    Response.Listener { response ->
+                        if (response=="0"){
+
+                        }
+                    },
+                    Response.ErrorListener { error ->  })
+
+
+            rq.add(sr)
     }
-}
+
+}}

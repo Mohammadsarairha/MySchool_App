77071ef15d85b2be79eebdca4684ccd81d7cfbf1
abdallah.sarayrah <abdallah.er11@gmail.com>
2018-04-16 18:14:46 +0300

    commit_abdallah

--- app/src/main/java/com/example/mohammad/myschool_app/Test1Fragment.kt
+++ app/src/main/java/com/example/mohammad/myschool_app/Test1Fragment.kt
@@ -6,6 +6,16 @@ import android.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_test1.view.*
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.example.mohammad.myschool_app.R.id.mapView
+import com.google.android.gms.maps.CameraUpdateFactory
+import sun.awt.windows.ThemeReader.getPosition
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.LatLngBounds
+
+
 
 
 // TODO: Rename parameter arguments, choose names that match
@@ -22,7 +32,13 @@ class Test1Fragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_test1, container, false)
+        val view = inflater.inflate(R.layout.fragment_test1, container, false)
+
+
+        view.mapView.getMapAsync { googleMap -> googleMap.moveCamera() }
+
+
+        return view
     }
 
 }

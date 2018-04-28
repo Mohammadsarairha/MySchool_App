15b22c52005a4cb3776c6cb946167b5dcfb02755
abdallah.sarayrah <abdallah.er11@gmail.com>
2018-04-28 18:11:20 +0300

    commit_abdallah

--- app/src/main/java/com/example/mohammad/myschool_app/MapTestFragment.kt
+++ app/src/main/java/com/example/mohammad/myschool_app/MapTestFragment.kt
@@ -6,6 +6,8 @@ import android.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
 
 
 // TODO: Rename parameter arguments, choose names that match
@@ -17,12 +19,17 @@ private const val ARG_PARAM2 = "param2"
  * A simple [Fragment] subclass.
  *
  */
-class MapTestFragment : Fragment() {
+class MapTestFragment : Fragment(), OnMapReadyCallback {
+    override fun onMapReady(p0: GoogleMap?) {
+
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map_test, container, false)
+        val view = inflater.inflate(R.layout.fragment_map_test, container, false)
+
+        return view
     }
 
 

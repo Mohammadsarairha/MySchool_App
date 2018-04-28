f2d62afc84d55a0b4313ab5b75a9f216f352a89d
abdallah.sarayrah <abdallah.er11@gmail.com>
2018-04-28 20:54:30 +0300

    commit_abdallah

--- app/src/main/java/com/example/mohammad/myschool_app/Test1Activity.kt
+++ app/src/main/java/com/example/mohammad/myschool_app/Test1Activity.kt
@@ -8,5 +8,9 @@ class Test1Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test1)
+
+        val tr = fragmentManager.beginTransaction()
+        val obj = MapTestFragment()
+        tr.replace(R.id.fragmentContainer, obj).addToBackStack(Test1Fragment().tag).commit()
     }
 }
--- app/src/main/java/com/example/mohammad/myschool_app/TestMapFragment.kt
+++ app/src/main/java/com/example/mohammad/myschool_app/TestMapFragment.kt
@@ -15,7 +15,11 @@ import com.google.android.gms.maps.MapsInitializer
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import kotlinx.android.synthetic.main.activity_test_maps.*
 import kotlinx.android.synthetic.main.fragment_map_test.*
+import kotlinx.android.synthetic.main.fragment_map_test.view.*
+import com.google.android.gms.maps.CameraUpdate
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException
 
 
 // TODO: Rename parameter arguments, choose names that match
@@ -29,38 +33,43 @@ private const val ARG_PARAM2 = "param2"
  */
 class MapTestFragment : Fragment(), OnMapReadyCallback {
 
+    val map: GoogleMap? = null
 
     @SuppressLint("MissingPermission")
     @RequiresApi(Build.VERSION_CODES.M)
     override fun onMapReady(p0: GoogleMap?) {
-        MapsInitializer.initialize(context)
-        p0?.mapType = GoogleMap.MAP_TYPE_NORMAL
+        val mMap = p0
 
         val jordan = LatLng(30.5852, 36.2384)
-        p0?.addMarker(MarkerOptions().position(jordan).title("Marker in Jordan"))
-        p0?.moveCamera(CameraUpdateFactory.newLatLng(jordan))
+        mMap?.addMarker(MarkerOptions().position(jordan).title("Marker in Jordan"))
+        mMap?.moveCamera(CameraUpdateFactory.newLatLng(jordan))
 
-        p0!!.isMyLocationEnabled = true
+        mMap?.isMyLocationEnabled = true
     }
 
-    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-
-        if (mapView != null) {
-            mapView.onCreate(null)
-            mapView.onResume()
-            mapView.getMapAsync(this)
-        }
-    }
-
+    @SuppressLint("MissingPermission")
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_map_test, container, false)
 
+        view.mapView.onCreate(savedInstanceState)
+        view.mapView.getMapAsync(this)
+
+        map?.uiSettings?.isMyLocationButtonEnabled = false
+        map?.isMyLocationEnabled = true
+
+        // Needs to call MapsInitializer before doing any CameraUpdateFactory calls
+        try {
+            MapsInitializer.initialize(this.activity)
+        } catch (e: GooglePlayServicesNotAvailableException) {
+            e.printStackTrace()
+        }
+
+        // Updates the location and zoom of the MapView
+        val cameraUpdate = CameraUpdateFactory.newLatLngZoom(LatLng(43.1, -87.9), 10f)
+        map?.animateCamera(cameraUpdate)
+
         return view
     }
-
-
 }

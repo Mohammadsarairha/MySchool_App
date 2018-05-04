1ad2d73c6d994fb01e31397219a2039409e501fe
abdallah.sarayrah <abdallah.er11@gmail.com>
2018-05-04 19:01:49 +0300

    abdallah_commit

--- app/src/main/java/com/example/mohammad/myschool_app/TestActivity.kt
+++ app/src/main/java/com/example/mohammad/myschool_app/TestActivity.kt
@@ -1,12 +1,18 @@
 package com.example.mohammad.myschool_app
 
 import android.Manifest
+import android.content.Context
 import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_test.*
+
 
 class TestActivity : AppCompatActivity() {
 
@@ -19,7 +25,28 @@ class TestActivity : AppCompatActivity() {
             ActivityCompat.requestPermissions(this,
                     arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 123)
         } else {
-            Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show()
+            val locationManager =
+                    getSystemService(Context.LOCATION_SERVICE)
+                            as LocationManager
+
+            val locationListener = object : LocationListener {
+                override fun onLocationChanged(location: Location?) {
+                    textView1.text = "lat:  ${location?.latitude}"
+                    textView2.text = "lng:  ${location?.longitude}"
+                }
+
+                override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
+                }
+
+                override fun onProviderEnabled(provider: String?) {
+                }
+
+                override fun onProviderDisabled(provider: String?) {
+                }
+            }
+
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0,
+                    0.0F, locationListener)
         }
     }
 

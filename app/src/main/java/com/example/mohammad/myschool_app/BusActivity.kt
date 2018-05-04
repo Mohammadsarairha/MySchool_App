2ded0ab15c4c6a32b1be049420430fa3419fbae6
abdallah.sarayrah <abdallah.er11@gmail.com>
2018-05-04 19:08:55 +0300

    abdallah_commit

--- app/src/main/java/com/example/mohammad/myschool_app/BusActivity.kt
+++ app/src/main/java/com/example/mohammad/myschool_app/BusActivity.kt
@@ -1,12 +1,17 @@
 package com.example.mohammad.myschool_app
 
+import android.Manifest
 import android.annotation.SuppressLint
 import android.content.Context
+import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
 import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_bus.*
 
@@ -17,7 +22,7 @@ class BusActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bus)
 
-        //Firebase reference
+        /*//Firebase reference
         val firebaseRef = FirebaseDatabase.getInstance()
                 .getReference("buses")
 
@@ -45,6 +50,56 @@ class BusActivity : AppCompatActivity() {
         }
 
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0,
-                0.0F, locationListener)
+                0.0F, locationListener)*/
+
+
+        if (ContextCompat.checkSelfPermission(this,
+                        Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 123)
+        } else {
+            //Firebase reference
+            val firebaseRef = FirebaseDatabase.getInstance()
+                    .getReference("buses")
+
+            val locationManager =
+                    getSystemService(Context.LOCATION_SERVICE)
+                            as LocationManager
+
+            val locationListener = object : LocationListener {
+                override fun onLocationChanged(location: Location?) {
+                    textView1.text = "lat:  ${location?.latitude}"
+                    textView2.text = "lng:  ${location?.longitude}"
+
+                    firebaseRef.child("bus1").child("lat").setValue(location?.latitude)
+                    firebaseRef.child("bus1").child("lng").setValue(location?.longitude)
+                }
+
+                override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
+                }
+
+                override fun onProviderEnabled(provider: String?) {
+                }
+
+                override fun onProviderDisabled(provider: String?) {
+                }
+            }
+
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0,
+                    0.0F, locationListener)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>,
+                                            grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            123 -> if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show()
+
+            } else
+                Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show()
+        }
     }
 }
